6b5bcae2061eb71c75ef87ccd31f74ae24fb112a
Paavo <paavo.nelimarkka@gmail.com>
2021-04-15 23:03:57 +0300

    FIX 2.4

--- part2/courseinfo2/src/index.js
+++ part2/courseinfo2/src/index.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = ({name}) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <ul>
+      {parts.map(parts => {
+        return (
+          <li>{parts.name}
+            <ul><li>Exercises: {parts.exercises}</li></ul>
+          </li>            
+          )
+        })
+      }
+    </ul>
+  )
+}
+
+const Total = ({ courses }) => {
+  return(
+    <strong>Total of exercises: {courses.parts.map(parts => parts.exercises).reduce((a, b) => a + b, 0)}</strong>
+  ) 
+}
+
 const Course = ({courses}) => {
     return (
     <div>
       {courses.map(courses => {
         return (
           <div key={courses.id}>
-            <h1>{courses.name}</h1>
-            <ul>
-              {courses.parts.map(parts => {
-                return (
-                  <li key={parts.id}>{parts.name}
-                    <ul><li>Exercises: {parts.exercises}</li></ul>
-                  </li>    
-                  )
-                })
-              }
-            </ul>
-            <strong>Total of exercises: {courses.parts.map(parts => parts.exercises).reduce((a, b) => a + b, 0)}</strong>
+            <Header name={courses.name} />
+            <Content parts={courses.parts} />
+            <Total courses={courses} />
           </div>
         )
       })
