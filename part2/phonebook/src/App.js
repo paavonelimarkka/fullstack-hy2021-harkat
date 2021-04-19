55e8cb447b5338904dd9a5086aa8199163aa4f7d
Paavo <paavo.nelimarkka@gmail.com>
2021-04-19 21:19:11 +0300

    add 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+const Names = ({numbers}) => {
+  return (
+    <ul>
+      {numbers.map(numbers => {
+        return (
+          <li key={numbers.name}>{numbers.name}</li>
+        )
+        })
+      }
+    </ul>
+  )
+}
+
+const App = () => {
+  const [newName, setNewName] = useState('')
+  const [numbers, setNumbers] = useState([
+    { name: 'Arto Hellas' }
+  ])
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => { 
+    event.preventDefault()
+    const nameObject = { name: newName }
+    setNumbers(numbers.concat(nameObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Debug zone</h2>
+      <p>NewName: {newName}</p>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addName} >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Names numbers={numbers} />
+      </div>
+  )
+}
+
+export default App
