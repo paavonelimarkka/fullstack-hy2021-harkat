81578f89782867602ab777658afd7f0507bc58b5
Paavo Nelimarkka <paavo.nelimarkka@jamk.fi>
2021-06-08 16:07:53 +0300

    add 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 
-const Names = ({numbers}) => {
+const Names = (props) => {
   return (
     <ul>
-      {numbers.map(numbers => {
+      {props.namesToShow.map(numbers => {
         return (
-          <li key={numbers.name}>{numbers.name} {numbers.number}</li>
+          <li key={numbers.name}>{numbers.name} - {numbers.number}</li>
         )
         })
       }
@@ -16,22 +16,33 @@ const Names = ({numbers}) => {
 const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
   const [numbers, setNumbers] = useState([
     {
       name: 'Arto Hellas',
       number: 122
+    },
+    {
+      name: 'Pasi Pasinpoika',
+      number: 187
     }
   ])
 
   const handleNameChange = (event) => { setNewName(event.target.value) }
   const handleNumberChange = (event) => { setNewNumber(event.target.value) }
+  const handleSearchChange = (event) => { setNewSearch(event.target.value) }
+
+  const filteredNames = numbers.filter(numbers => numbers.name.toLowerCase().includes(newSearch.toLowerCase()))
+
+  const namesToShow = filteredNames.length > 0
+    ? filteredNames
+    : numbers
 
   const addData = (event) => { 
     event.preventDefault()
 
     if (numbers.some(e => e.name === newName && newName && newNumber)) {
       alert(newName + " is already added to phonebook")
-      console.log("Miau")
     }
     else {
       const nameObject = {
@@ -47,21 +58,24 @@ const App = () => {
 
   return (
     <div>
-      <h2>Debug zone</h2>
-      <p>NewName: {newName}</p>
-      <p>NewNumber: {newNumber}</p>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
       <form>
-        <div> name: <input value={newName} onChange={handleNameChange} /></div>
-        <div> number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div> Name: <input value={newName} onChange={handleNameChange} /></div>
+        <div> Number: <input value={newNumber} onChange={handleNumberChange} /></div>
 
         <div>
-          <button type="submit" onClick={addData} >add</button>
+          <button type="submit" onClick={addData} >Add</button>
         </div>
       </form>
+
+      <h2>Search users</h2>
+      <form>
+        <input value={newSearch} onChange={handleSearchChange} />
+      </form>
+
       <h2>Numbers</h2>
-      <Names numbers={numbers} />
-      </div>
+      <Names numbers={numbers} namesToShow={namesToShow} />
+    </div>
   )
 }
 
